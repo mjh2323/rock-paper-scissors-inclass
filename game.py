3f86dbe0ced4df9c7f7f3ab669176e457e9c6123
mjh2323 <77938125+mjh2323@users.noreply.github.com>
2021-02-03 18:21:34 -0500

    validate user inputs

--- game.py
+++ game.py
@@ -28,6 +28,52 @@ computer_choice = random.choice(options)
 
 print("The computer chose: {computer_choice}")
 
+
+#
+# validate user selection 
+#
+# stop the program and not try to determine the winner)
+# if the user choice is invalid
+
+#
+# determining who won 
+#
+
+if user_choice == computer_choice:
+    print("It's a tie!")
+
+if user_choice == "rock":
+    if computer_choice == "rock":
+        print("Oh, it's a tie.")
+    elif computer_choice == "paper":
+        print("Oh, the computer won. It's ok.")
+    elif computer_choice == "scissors":
+        print("Oh, you won! Nice job.")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("Oh, you won! Nice job.")
+    elif computer_choice == "paper":
+        print("Oh, it's a tie.")
+    elif computer_choice == "scissors":
+        print("Oh, the computer won. It's ok.")
+elif user_choice == "scissors":
+    if computer_choice == "rock":
+        print("Oh, the computer won. It's ok.")
+    elif computer_choice == "paper":
+        print("Oh, you won! Nice job.")
+    elif computer_choice == "scissors":
+        print("Oh, it's a tie.")
+else:
+    print("OOPS SOMETHING WENT WRONG.")
+
+
+
+
+
+
+
+
+
 exit()
 
 
