93eb1738f9c3664d5ae8787ab26efd9351b77372
mjh2323 <77938125+mjh2323@users.noreply.github.com>
2021-02-03 17:40:10 -0500

    ask user for input

--- game.py
+++ game.py
@@ -1,3 +1,23 @@
 # game.py
 
 print("Rock, Paper, Scissors, Shoot!")
+
+
+
+print("-------------------")
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print("-------------------")
+
+
+
+
+x = input("Please choose either 'rock', 'paper', or 'scissors': ")
+
+print(x)
+print("You chose: ", x)
+
+
+
+exit()
+
+
